3791fa8e2d25c54a77b779b0ce0445e65b5f7742
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-10-05 19:39:46 +0200

    Added player input and intertia to x movement.

--- index.js
+++ index.js
@@ -7,15 +7,57 @@ canvas.height = 64 * 9;
 
 const player = new Player()
 
+const actions = {
+    moveRight: {
+        key: 'KeyD',
+        pressed: false,
+    },
+    moveLeft: {
+        key: 'KeyA',
+        pressed: false,
+    },
+};
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-
     c.fillStyle = 'gray';
     c.fillRect(0, 0, canvas.width, canvas.height);
+
+    if (actions.moveRight.pressed) player.accelerateRight();
+    else if (actions.moveLeft.pressed) player.accelerateLeft();
+    else player.deccelerate();
 
     player.update();
     player.draw();
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'Space':
+            if (player.vel.y == 0) {
+                console.log("Jump");
+                player.vel.y = -20; };
+            break
+        case actions.moveRight.key:
+            actions.moveRight.pressed = true;
+            break
+        case actions.moveLeft.key:
+            actions.moveLeft.pressed = true;
+            break
+        default:
+            console.log("Unknown key pressed")
+            console.log(event.code)
+}})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case actions.moveRight.key:
+            actions.moveRight.pressed = false;
+            break
+        case actions.moveLeft.key:
+            actions.moveLeft.pressed = false;
+            break
+}})
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -9,10 +9,11 @@ class Player {
             y: -10
         };
         this.acc = {
-            x: 2.0,
-            y: 10
+            x: 1.5,
+            y: 1
         };
-
+        
+        this.speed = 10;
         this.width = 100;
         this.height = 100;
         
@@ -23,10 +24,33 @@ class Player {
         c.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     };
 
+    accelerateRight() {
+        if (this.vel.x + this.acc.x < this.speed) {
+            if (this.vel.y === 0) this.vel.x += this.acc.x;
+            else this.vel.x += this.acc.x / 5;  // Less x controll mid-air
+        } else {this.vel.x = this.speed}};
+
+    accelerateLeft() {
+        if (this.vel.x - this.acc.x > -this.speed) {
+            if (this.vel.y === 0) this.vel.x -= this.acc.x;
+            else this.vel.x -= this.acc.x / 5;  // Less x controll mid-air
+        } else {this.vel.x = -this.speed}};
+
+    deccelerate() {
+        if (this.vel.x > this.acc.x) {
+            if (this.vel.y === 0) this.vel.x -= this.acc.x;
+            else this.vel.x -= this.acc.x / 8;  // Less x controll mid-air
+        } else if (this.vel.x < -this.acc.x) {
+            if (this.vel.y === 0) this.vel.x += this.acc.x;
+            else this.vel.x += this.acc.x / 8;  // Less x controll mid-air
+        } else {this.vel.x = 0}};
+
     update() {
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
+
         if (player.pos.y + player.height < canvas.height){
-            player.vel.y += player.acc.y / 30;
-            player.pos.y += player.vel.y;
+            player.vel.y += player.acc.y;
         }
         else {
             player.pos.y = canvas.height - player.height;
