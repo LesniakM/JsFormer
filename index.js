d509b62033f24abe798dc16c72abd2967908e672
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-10-31 18:46:22 +0100

    Added loading progress.

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-import { startScreen, loadingFinished } from './js/utils.js';
-import { canvas, context } from './js/Canvas.js';
-import ImageContainer from './js/classes/ImageContainer.js';
+import {
+  canvas, context, startScreen, loadingFinished, loadingProgress,
+} from './js/Canvas.js';
 import { WorldSounds } from './data/audio.js';
 import Game from './js/classes/Game.js';
 
 startScreen();
-const images = new ImageContainer();
-export default images;
 const sounds = new WorldSounds();
 sounds.bgmusic.play();
 const game = new Game(canvas, context, sounds);
-loadingFinished();
+
+let imageLoadProgress = 0;
+let audioLoadProgress = 0;
+let gameIsReady = false;
 
 function mainLoop() {
   window.requestAnimationFrame(mainLoop);
-  game.gameLoop();
-  if (actions.any.pressed === true) game.startGame();
+  if (game.gameStarted) {
+    game.gameLoop();
+  } else {
+    if (!gameIsReady) {
+      imageLoadProgress = game.images.loadProgress();
+      audioLoadProgress = game.images.loadProgress();
+      loadingProgress((imageLoadProgress + audioLoadProgress) / 2);
+      if (imageLoadProgress === 1 && audioLoadProgress === 1) {
+        gameIsReady = true;
+        loadingFinished();
+      }
+    }
+    if (actions.any.pressed === true && gameIsReady) game.startGame();
+  }
 }
 
 mainLoop();
