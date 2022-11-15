3b7c8ec13c040e0f61ca2e4002fb3bf7df27e759
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-11-15 20:34:12 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-import {
-  canvas, context, startScreen, loadingFinished, loadingProgress,
-} from './js/Canvas.js';
-import Game from './js/classes/Game.js';
-
-startScreen();
-// sounds.bgmusic.play();
-const game = new Game(canvas, context);
-
-let imageLoadProgress = 0;
-let audioLoadProgress = 0;
-let gameIsReady = false;
-
-function mainLoop() {
+export default function mainLoop() {
   window.requestAnimationFrame(mainLoop);
-  if (game.gameStarted) {
-    game.gameLoop();
-  } else {
-    if (!gameIsReady) {
-      imageLoadProgress = game.images.loadProgress();
-      audioLoadProgress = game.sounds.loadProgress();
-      loadingProgress((imageLoadProgress + audioLoadProgress) / 2);
-      if (imageLoadProgress === 1 && audioLoadProgress === 1) {
-        gameIsReady = true;
-        loadingFinished();
-      }
-    }
-    if (actions.any.pressed === true && gameIsReady) game.startGame();
-  }
+  console.log('mainloop');
 }
-
 mainLoop();
