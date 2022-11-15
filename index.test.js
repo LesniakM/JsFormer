5fe4798c68866f4d46973d2022c39910f4368a20
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-11-15 20:34:38 +0100

    Refactoring.

---
+++ index.test.js
@@ -0,0 +1,5 @@
+import mainLoop from './index.js';
+
+test('Check if mainLoop function exists', () => {
+  expect(typeof mainLoop).toEqual('function');
+});
--- testTemplate.test.js
+++
@@ -1,5 +0,0 @@
-import sum from './testTemplate.js';
-
-test('adds 3 + 2 to equal 5', () => {
-  expect(sum(3, 2)).toBe(5);
-});
